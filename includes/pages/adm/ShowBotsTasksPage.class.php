731f55f305bc0be41c5b8a97e0967019616df7ec
f2fossil <f2fossil@gmx.de>
2026-02-20 17:45:53 +0100

    Admin bot info

--- includes/pages/adm/ShowBotsTasksPage.class.php
+++ includes/pages/adm/ShowBotsTasksPage.class.php
@@ -17,15 +17,7 @@ class ShowBotsTasksPage extends AbstractAdminPage
         $task = HTTP::_GP('task', '', true);
 
         $allowedTasks = [
-            'attack_idlers',
-            'attack_players',
-            'autobuilder',
-            'autorefresher',
-            'build_ships',
-            'colonize_planets',
-            'remove_messages',
-            'send_expedition',
-            'send_recyclers'
+            
         ];
 
         if (!in_array($task, $allowedTasks)) {
@@ -46,152 +38,184 @@ class ShowBotsTasksPage extends AbstractAdminPage
     /* =========================
      * 🤖 AI BOT INFO + 🔬 Forschung
      * ========================= */
-    function aiinfo()
+   function aiinfo()
+{
+    $db = Database::get();
+    global $LNG, $resource;
+
+$rows = $db->select(
+    "SELECT
+        u.id,
+        u.username,
+        u.bot_next_action,
+
+        -- Forschung Queue
+        u.b_tech,
+        u.b_tech_id,
+        u.b_tech_planet,
+        u.b_tech_queue,
+
+        -- Forschungslevel
+        u.spy_tech,
+        u.computer_tech,
+        u.military_tech,
+        u.defence_tech,
+        u.shield_tech,
+        u.energy_tech,
+        u.hyperspace_tech,
+        u.combustion_tech,
+        u.impulse_motor_tech,
+        u.hyperspace_motor_tech,
+        u.laser_tech,
+        u.ionic_tech,
+        u.buster_tech,
+        u.intergalactic_tech,
+        u.expedition_tech,
+
+        -- Planet
+        p.id AS planet_id,
+        p.name AS planet_name,
+        p.galaxy, p.system, p.planet,
+
+        p.metal,
+        p.crystal,
+        p.deuterium,
+
+        p.metal_mine,
+        p.crystal_mine,
+        p.deuterium_sintetizer,
+        p.solar_plant,
+        p.energy,
+        p.energy_used,
+
+        p.robot_factory,
+        p.hangar,
+        p.laboratory,
+
+        p.metal_store,
+        p.crystal_store,
+        p.deuterium_store,
+
+        p.misil_launcher,
+        p.small_laser,
+        p.big_laser,
+        p.gauss_canyon,
+        p.ionic_canyon,
+        p.buster_canyon,
+        p.small_protection_shield,
+        p.big_protection_shield,
+
+        p.b_building
+
+    FROM ".DB_PREFIX."users u
+    LEFT JOIN ".DB_PREFIX."planets p
+        ON p.id_owner = u.id
+       AND p.planet_type = 1
+    WHERE u.is_bot = 1
+    ORDER BY u.id ASC"
+);
+
+    $now = time();
+    $bots = [];
+
+    foreach ($rows as $row)
     {
-        $db = Database::get();
-        global $LNG, $resource;
+        $uid = $row['id'];
 
-        $bots = $db->select(
-            "SELECT
-                u.id,
-                u.username,
-                u.bot_next_action,
-
-                -- 🔬 Forschung Queue
-                u.b_tech,
-                u.b_tech_id,
-                u.b_tech_planet,
-                u.b_tech_queue,
-
-                -- 🔬 Forschungslevel (NUR existierende Standard-Spalten)
-                u.spy_tech,
-                u.computer_tech,
-                u.military_tech,
-                u.defence_tech,
-                u.shield_tech,
-                u.energy_tech,
-                u.hyperspace_tech,
-                u.combustion_tech,
-                u.impulse_motor_tech,
-                u.hyperspace_motor_tech,
-                u.laser_tech,
-                u.ionic_tech,
-                u.buster_tech,
-                u.intergalactic_tech,
-                u.expedition_tech,
-
-                p.id AS planet_id,
-                p.name AS planet_name,
-                p.galaxy, p.system, p.planet,
-
-                -- Ressourcen
-                p.metal, p.crystal, p.deuterium,
-
-                -- Minen
-                p.metal_mine,
-                p.crystal_mine,
-                p.deuterium_sintetizer,
-
-                -- Energie
-                p.solar_plant,
-                p.energy,
-                p.energy_used,
-
-                -- Infrastruktur
-                p.metal_store,
-                p.crystal_store,
-                p.deuterium_store,
-                p.robot_factory,
-                p.hangar,
-                p.laboratory,
-
-                -- Verteidigung
-                p.misil_launcher,
-                p.small_laser,
-                p.big_laser,
-                p.gauss_canyon,
-                p.ionic_canyon,
-                p.buster_canyon,
-                p.small_protection_shield,
-                p.big_protection_shield,
-
-                -- Bauqueue
-                p.b_building
-
-            FROM ".DB_PREFIX."users u
-            INNER JOIN ".DB_PREFIX."planets p
-              ON p.id_owner = u.id
-             AND p.planet_type = 1
-            WHERE u.is_bot = 1
-            ORDER BY u.id ASC"
-        );
-
-        $now = time();
-
-        foreach($bots as &$bot)
+        /* ===== Bot einmal anlegen ===== */
+        if (!isset($bots[$uid]))
         {
-            /* ============== ⚡ Energie & Bau ============== */
-            $bot['energy_free'] = $bot['energy'] - $bot['energy_used'];
+            $bots[$uid] = $row;
+            $bots[$uid]['planets'] = [];
+            $bots[$uid]['next_action'] = !empty($row['bot_next_action'])
+    ? date('H:i:s', $row['bot_next_action'])
+    : '-';
 
-            $bot['build_status'] =
-                ($bot['b_building'] > $now)
-                ? 'läuft bis '.date('H:i:s', $bot['b_building'])
-                : 'frei';
-
-            $bot['next_action'] =
-                $bot['bot_next_action']
-                ? date('H:i:s', $bot['bot_next_action'])
-                : '-';
-
-
-            /* ============== 🔬 Forschungs-Levels sammeln ============== */
-
+            /* ===== Forschung bauen ===== */
             $researchIds = [
                 106,108,109,110,111,
                 113,114,115,117,118,
                 120,121,122,123,124
             ];
 
-            $bot['research'] = [];
+            $bots[$uid]['research'] = [];
 
-            foreach($researchIds as $rid)
+            foreach ($researchIds as $rid)
             {
-                if (!isset($resource[$rid])) {
-                    continue;
-                }
+                if (!isset($resource[$rid])) continue;
 
                 $col = $resource[$rid];
 
-                $bot['research'][$rid] = [
+                $bots[$uid]['research'][$rid] = [
                     'name'  => $LNG['tech'][$rid] ?? ('Tech '.$rid),
-                    'level' => (int)($bot[$col] ?? 0),
+                    'level' => (int)($row[$col] ?? 0),
                 ];
             }
 
-            /* ============== 🟡 Laufende Forschung ============== */
+            /* ===== Aktive Forschung ===== */
+            $bots[$uid]['research_active'] = null;
 
-            $bot['research_active'] = null;
+            if (!empty($row['b_tech']) && $row['b_tech'] > $now && $row['b_tech_id'] > 0)
+            {
+                $tid = (int)$row['b_tech_id'];
 
-            if (
-                !empty($bot['b_tech'])
-                && $bot['b_tech'] > $now
-                && $bot['b_tech_id'] > 0
-            ) {
-                $tid = (int)$bot['b_tech_id'];
-
-                $bot['research_active'] = [
+                $bots[$uid]['research_active'] = [
                     'id'   => $tid,
                     'name' => $LNG['tech'][$tid] ?? ('Tech '.$tid),
-                    'end'  => date('H:i:s', $bot['b_tech']),
+                    'end'  => date('H:i:s', $row['b_tech']),
                 ];
             }
         }
 
-        $this->tplObj->assign_vars([
-            'bots' => $bots,
-        ]);
+        /* ===== Planeten anhängen ===== */
+        if (!empty($row['planet_id']))
+        {
+ $bots[$uid]['planets'][] = [
+    'planet_id' => $row['planet_id'],
+    'name'      => $row['planet_name'],
+    'coords'    => $row['galaxy'].':'.$row['system'].':'.$row['planet'],
 
-        $this->display('page.bot.aiinfo.tpl');
+    'metal'     => $row['metal'],
+    'crystal'   => $row['crystal'],
+    'deuterium' => $row['deuterium'],
+
+    'metal_mine' => $row['metal_mine'],
+    'crystal_mine' => $row['crystal_mine'],
+    'deut_synth' => $row['deuterium_sintetizer'],
+
+    'solar'  => $row['solar_plant'],
+    'energy' => $row['energy'] - $row['energy_used'],
+
+    'robot' => $row['robot_factory'],
+    'hangar' => $row['hangar'],
+    'lab' => $row['laboratory'],
+    
+    'metal_store' => $row['metal_store'],
+'crystal_store' => $row['crystal_store'],
+'deut_store' => $row['deuterium_store'],
+
+'ml' => $row['misil_launcher'],
+'sl' => $row['small_laser'],
+'bl' => $row['big_laser'],
+'ga' => $row['gauss_canyon'],
+'io' => $row['ionic_canyon'],
+'pl' => $row['buster_canyon'],
+'ss' => $row['small_protection_shield'],
+'bs' => $row['big_protection_shield'],
+
+    'build_end' => $row['b_building'],
+    'build_status' => ($row['b_building'] > $now)
+        ? 'läuft bis '.date('H:i:s', $row['b_building'])
+        : 'frei'
+];
+        }
     }
+
+    $this->tplObj->assign_vars([
+        'bots' => array_values($bots),
+    ]);
+
+    $this->display('page.bot.aiinfo.tpl');
 }
 
+}
--- styles/templates/adm/page.bot.aiinfo.tpl
+++ styles/templates/adm/page.bot.aiinfo.tpl
@@ -1,221 +1,141 @@
 {block name="content"}
 <div class="bg-black w-95 text-white p-3 my-3 mx-auto fs-12">
 
-    <h2 class="text-yellow mb-3">🤖 AI-Bot Übersicht</h2>
+<h2 class="text-yellow mb-3">🤖 AI-Bot Übersicht</h2>
 
-    <table class="table table-dark table-sm table-bordered text-center">
-        <thead>
-            <tr class="text-yellow">
-                <th>Bot</th>
-                <th>Planet</th>
-                <th>Koords</th>
+<table class="table table-dark table-sm table-bordered text-center">
+<thead>
+<tr class="text-yellow">
+    <th>Bot</th>
+    <th>Planeten</th>
+    <th>Ressourcen</th>
+    <th>Bauqueue</th>
+    <th>Nächste Aktion</th>
+</tr>
+</thead>
 
-                <th>Metal</th>
-                <th>Kristall</th>
-                <th>Deut</th>
+<tbody>
+{foreach $bots as $bot}
+<tr>
 
-                <th>Metal-Mine</th>
-                <th>Kristall-Mine</th>
-                <th>Deut-Synth</th>
+<td class="text-yellow">{$bot.username}</td>
 
-                <th>Solar</th>
-                <th>Energie</th>
+<td class="text-left">
+{foreach $bot.planets as $p}
+    {$p.name} [{$p.coords}]<br>
+{/foreach}
+</td>
 
-                <th>M-Speicher</th>
-                <th>K-Speicher</th>
-                <th>D-Speicher</th>
+<td class="text-left">
 
-                <th>Robot</th>
-                <th>Werft</th>
-                <th>Labor</th>
+{foreach $bot.planets as $p}
+
+<div style="border-bottom:1px solid #333; margin-bottom:6px; padding-bottom:4px;">
+
+<b class="text-yellow">{$p.name}</b>
+<span style="color:#888">[{$p.coords}]</span><br>
+
+M {$p.metal|number_format}
+K {$p.crystal|number_format}
+D {$p.deuterium|number_format}<br>
+
+Mine {$p.metal_mine}/{$p.crystal_mine}/{$p.deut_synth}<br>
+
+Solar {$p.solar}
+{if $p.energy < 0}
+<span class="text-red">({$p.energy})</span>
+{else}
+<span class="text-green">({$p.energy})</span>
+{/if}
+<br>
+
+Speicher {$p.metal_store}/{$p.crystal_store}/{$p.deut_store}<br>
+
+Robo {$p.robot} Werft {$p.hangar} Lab {$p.lab}<br>
+
+Defense:
+R {$p.ml}
+KL {$p.sl}
+SL {$p.bl}
+G {$p.ga}
+I {$p.io}
+P {$p.pl}
+KS {$p.ss}
+GS {$p.bs}
+
+</div>
+
+{/foreach}
+
+</td>
+
+<td class="text-left">
+{foreach $bot.planets as $p}
+    {if $p.build_end > time()}
+        <span class="text-orange">{$p.build_status}</span><br>
+    {else}
+        <span class="text-green">frei</span><br>
+    {/if}
+{/foreach}
+</td>
+
+<td>{$bot.next_action}</td>
+
+</tr>
+{/foreach}
+</tbody>
+</table>
 
 
-                <th>Raketen</th>
-                <th>K-Laser</th>
-                <th>S-Laser</th>
-                <th>Gauss</th>
-                <th>Ion</th>
-                <th>Plasma</th>
-                <th>K-Schild</th>
-                <th>G-Schild</th>
-
-                <th>Defense-Score</th>
-
-                <th>Bau</th>
-                <th>Nächste Aktion</th>
-            </tr>
-        </thead>
-
-        <tbody>
-        {foreach $bots as $bot}
-
-            {* ==== Defense Defaults ==== *}
-            {assign var=ml value=$bot.misil_launcher|default:0}
-            {assign var=sl value=$bot.small_laser|default:0}
-            {assign var=bl value=$bot.big_laser|default:0}
-            {assign var=ga value=$bot.gauss_canyon|default:0}
-            {assign var=io value=$bot.ionic_canyon|default:0}
-            {assign var=pl value=$bot.buster_canyon|default:0}
-            {assign var=ss value=$bot.small_protection_shield|default:0}
-            {assign var=bs value=$bot.big_protection_shield|default:0}
-
-            {* ==== Defense Score ==== *}
-            {assign var=defscore value=
-                $ml*1 +
-                $sl*2 +
-                $bl*3 +
-                $io*4 +
-                $ga*6 +
-                $pl*10 +
-                $ss*20 +
-                $bs*40
-            }
-
-            {* ==== 🧪 Research Mapping ==== *}
-            {assign var=techName value=$bot.tech_name|default:'-'}
-
-            <tr>
-                <td>{$bot.username}</td>
-                <td>{$bot.planet_name}</td>
-                <td>[{$bot.galaxy}:{$bot.system}:{$bot.planet}]</td>
-
-                <td>{$bot.metal|number_format}</td>
-                <td>{$bot.crystal|number_format}</td>
-                <td>{$bot.deuterium|number_format}</td>
-
-                <td>{$bot.metal_mine}</td>
-                <td>{$bot.crystal_mine}</td>
-                <td>{$bot.deuterium_sintetizer}</td>
-
-                <td>{$bot.solar_plant}</td>
-                <td>{$bot.energy - $bot.energy_used}</td>
-
-                <td>{$bot.metal_store}</td>
-                <td>{$bot.crystal_store}</td>
-                <td>{$bot.deuterium_store}</td>
-
-                <td>{$bot.robot_factory}</td>
-                <td>{$bot.hangar}</td>
-                <td>{$bot.laboratory}</td>
-
-               
-
-                {* ==== Defense Values ==== *}
-                <td>{$ml}</td>
-                <td>{$sl}</td>
-                <td>{$bl}</td>
-                <td>{$ga}</td>
-                <td>{$io}</td>
-                <td>{$pl}</td>
-                <td>{$ss}</td>
-                <td>{$bs}</td>
-
-                {* ==== Defense Rating ==== *}
-                <td>
-                    {if $defscore == 0}
-                        <span class="text-red">Keine</span>
-                    {elseif $defscore < 50}
-                        <span class="text-orange">Schwach ({$defscore})</span>
-                    {elseif $defscore < 200}
-                        <span class="text-yellow">OK ({$defscore})</span>
-                    {else}
-                        <span class="text-green">Gut ({$defscore})</span>
-                    {/if}
-                </td>
-
-                <td>{$bot.build_status}</td>
-                <td>{$bot.next_action}</td>
-
-            </tr>
-        {/foreach}
-        </tbody>
-    </table>
-    
-   <h3 class="text-yellow mt-4 mb-2">🔬 Forschungsübersicht</h3>
+<h3 class="text-yellow mt-4 mb-2">🔬 Forschungsübersicht</h3>
 
 <table class="table table-dark table-sm table-bordered text-center">
 
 <thead>
 <tr class="text-yellow">
     <th>Bot</th>
-    <th>Fertig um</th>
-
+    <th>Aktiv</th>
     <th>Spio</th>
     <th>Computer</th>
     <th>Militär</th>
-    <th>Verteidigung</th>
     <th>Schild</th>
-
     <th>Energie</th>
-    <th>Hyperraum</th>
+    <th>Hyper</th>
     <th>Verbrennung</th>
     <th>Impuls</th>
     <th>Hyperantrieb</th>
-
-    <th>Laser</th>
-    <th>Ionen</th>
-    <th>Plasma</th>
-
-    <th>IG-Netz</th>
-    <th>Expedition</th>
-
-    <th>M-Prod</th>
-    <th>K-Prod</th>
-    <th>D-Prod</th>
 </tr>
 </thead>
 
 <tbody>
-
 {foreach $bots as $bot}
-
 <tr>
 
 <td>{$bot.username}</td>
 
-{* --- Aktive Forschung --- *}
-
+<td>
 {if $bot.research_active}
-    <td>
-        {$bot.research_active.name}
-        <br>
-        <small>{$bot.research_active.end}</small>
-    </td>
+    <span class="text-yellow">{$bot.research_active.name}</span><br>
+    <small>{$bot.research_active.end}</small>
 {else}
-    <td><span class="text-red">-</span></td>
+    <span class="text-gray">frei</span>
 {/if}
-
-{* --- Levelmatrix --- *}
+</td>
 
 <td>{$bot.research.106.level|default:0}</td>
 <td>{$bot.research.108.level|default:0}</td>
 <td>{$bot.research.109.level|default:0}</td>
-<td>{$bot.research.110.level|default:0}</td>
 <td>{$bot.research.111.level|default:0}</td>
-
 <td>{$bot.research.113.level|default:0}</td>
 <td>{$bot.research.114.level|default:0}</td>
 <td>{$bot.research.115.level|default:0}</td>
 <td>{$bot.research.117.level|default:0}</td>
 <td>{$bot.research.118.level|default:0}</td>
 
-<td>{$bot.research.120.level|default:0}</td>
-<td>{$bot.research.121.level|default:0}</td>
-<td>{$bot.research.122.level|default:0}</td>
-
-<td>{$bot.research.123.level|default:0}</td>
-<td>{$bot.research.124.level|default:0}</td>
-
-<td>{$bot.research.131.level|default:0}</td>
-<td>{$bot.research.132.level|default:0}</td>
-<td>{$bot.research.133.level|default:0}</td>
-
 </tr>
-
 {/foreach}
-
 </tbody>
-</table> 
+</table>
 
 </div>
 {/block}
